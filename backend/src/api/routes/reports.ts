807879101286f5c38a1124964803f6f4039fb04f
Adam Cardenas <cevaris@gmail.com>
2020-09-01 14:50:31 +0000

    correctly render status report latest failures

--- backend/src/api/routes/reports.ts
+++ backend/src/api/routes/reports.ts
@@ -15,29 +15,27 @@ const MaxLatestFailures = 10;
 
 router.get('/reports/:name.json', async function (req: ReportsNameRequest, res: express.Response) {
 
-    let query: Promise<Array<StatusReport>> = Promise.resolve([]);
-    if (req.query.latest_failures) {
-        let latestFailures = parseInt(req.query.latest_failures);
-        if (isNaN(latestFailures)) {
-            return res.status(401)
-                .json({ ok: false, message: `latest_failures value '${req.query.latest_failures}' is not a number` })
-        }
-        if (Number.isInteger(latestFailures) && latestFailures > MaxLatestFailures) {
-            return res.status(401)
-                .json({ ok: false, message: `latest_failures value '${latestFailures}' cannot be larger than ${MaxLatestFailures}` })
-        }
-
-        query = StatusReportStore.getLastErrors(req.params.name, latestFailures)
-    } else {
-        query = StatusReportStore.getLastHour(req.params.name);
-    }
-
     try {
-        const entities: Array<StatusReport> = await query;
-        entities.sort((a, b) => (a.startDate > b.startDate) ? 1 : -1);
+        let entities: Array<StatusReport> = [];
+        if (req.query.latest_failures) {
+            let latestFailures = parseInt(req.query.latest_failures);
+            if (isNaN(latestFailures)) {
+                return res.status(401)
+                    .json({ ok: false, message: `latest_failures value '${req.query.latest_failures}' is not a number` })
+            }
+            if (Number.isInteger(latestFailures) && latestFailures > MaxLatestFailures) {
+                return res.status(401)
+                    .json({ ok: false, message: `latest_failures value '${latestFailures}' cannot be larger than ${MaxLatestFailures}` })
+            }
+
+            entities = await StatusReportStore.getLastErrors(req.params.name, latestFailures)
+        } else {
+            entities = await StatusReportStore.getLastHour(req.params.name);
+            entities.sort((a, b) => (a.startDate > b.startDate) ? 1 : -1);
+        }
 
         res.type('json')
-            .send(renderJson(entities));
+            .send(entities);
     } catch (error) {
         return res.status(503)
             .json({ ok: false, message: error.message });
