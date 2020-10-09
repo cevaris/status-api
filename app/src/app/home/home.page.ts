62798dc6f0e4286b10e06701b22dc5f2d80b6b17
Adam Cardenas <cevaris@gmail.com>
2020-10-09 01:35:35 +0000

    render report key in front page

--- app/src/app/home/home.page.ts
+++ app/src/app/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import axios from 'axios';
 import { environment } from 'src/environments/environment';
-import { StatusReport } from '..';
+import { StatusReport, StatusReportMetadata } from '..';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +10,7 @@ import { StatusReport } from '..';
 })
 export class HomePage {
   public latestFailures: Array<StatusReport>;
+  // public metadata: Map<String, StatusReportMetadata>;
 
   constructor() { }
 
@@ -24,6 +25,15 @@ export class HomePage {
     try {
       const latestFailuresResponse = await axios.get<StatusReport[]>(`${environment.apiHost}/reports/failures.json`);
       this.latestFailures = latestFailuresResponse.data;
+
+      // const metadataResponse = await axios.get<Array<StatusReportMetadata>>(`${environment.apiHost}/report_metadata.json`);
+      // if (metadataResponse.data?.length > 0) {
+      //   this.metadata = new Map(metadataResponse.data.map(m => [m.key, m]));
+      //   console.log(this.metadata);
+      // } else {
+      //   console.error('StatusReportMetadata query returned no result');
+      // }
+
     } catch (error) {
       console.error(error);
     }
