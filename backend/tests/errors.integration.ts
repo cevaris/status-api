0b9f7cf15133bb0a1f448b8480214d4a094e8e52
Adam Cardenas <cevaris@gmail.com>
2020-10-09 03:14:55 +0000

    added all 3 error reports

--- backend/tests/errors.integration.ts
+++ backend/tests/errors.integration.ts
@@ -1,23 +1,35 @@
 import { exec } from 'child_process';
-import { json } from 'express';
-import { join } from 'path';
 import { promisify } from 'util';
 const execPromise = promisify(exec);
 
 describe('errors', () => {
     it('errors:global:debug:random_throw', async () => {
         const key = 'errors:global:debug:random_throw';
-        const command = `node ./dist/src/runner/adhoc.js ${key}`;
-
-        const response = await execPromise(command);
-        const report = JSON.parse(response.stdout);
-
-        expect(report['name']).toBe(key);
-        expect(typeof report['ok']).toBe('boolean');
-        expect(typeof report['latencyMs']).toBe('number');
-
-        if (!report['ok']) {
-            expect(report['message']).toBe('Error: this error is intentional');
-        }
+        execute(key, 'Error: this error is intentional');
     });
+
+    it('errors:global:debug:throw', async () => {
+        const key = 'errors:global:debug:throw';
+        execute(key, 'Error: this error is intentional');
+    });
+
+    it('errors:global:debug:timeout', async () => {
+        const key = 'errors:global:debug:timeout';
+        execute(key, /Error: Report runner timed-out, \d+ms/);
+    }, 11000);
 });
+
+async function execute(key: string, failureMessage: string | RegExp) {
+    const command = `node ./dist/src/runner/adhoc.js ${key}`;
+
+    const response = await execPromise(command);
+    const report = JSON.parse(response.stdout);
+
+    expect(report['name']).toBe(key);
+    expect(typeof report['ok']).toBe('boolean');
+    expect(typeof report['latencyMs']).toBe('number');
+
+    if (!report['ok']) {
+        expect(report['message']).toMatch(failureMessage);
+    }
+}
