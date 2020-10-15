88ce83bf7c1e0a5801756a981bee5fd153653083
Adam Cardenas <cevaris@gmail.com>
2020-10-15 02:01:46 +0000

    working on header

--- app/src/app/home/home.module.ts
+++ app/src/app/home/home.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { HeaderComponentModule } from '../shared/header/heder.module';
+import { HeaderComponentModule } from '../components/header/heder.module';
 import { HomePageRoutingModule } from './home-routing.module';
 import { HomePage } from './home.page';
 
