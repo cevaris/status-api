fb5f936faef9a65e1f391b846783ec90fb365ba2
Adam Cardenas <cevaris@gmail.com>
2020-10-16 14:40:36 +0000

    working resizeable menu

--- app/src/app/components/header/header.component.ts
+++ app/src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { MenuController } from '@ionic/angular';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { MenuController, Platform } from '@ionic/angular';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 // https://devdactic.com/horizontal-navigation-ionic-desktop/
 
@@ -10,11 +12,34 @@ import { MenuController } from '@ionic/angular';
 export class HeaderComponent implements OnInit {
   @Input() displayBackArrow: boolean;
 
-  constructor(private menu: MenuController) {
+  private isDesktop = new BehaviorSubject<boolean>(false);
+
+  constructor(private menu: MenuController, private platform: Platform) {
   }
   ngOnInit(): void {
     // this.menu.enable(true, 'first');
     // this.menu.open('first');
+
+    this.resize(this.platform.width());
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.resize(event.target.innerWidth);
+  }
+
+  private resize(size: number) {
+    if (size < 568) {
+      this.isDesktop.next(false);
+    } else {
+      this.isDesktop.next(true);
+    }
+
+    console.log(this.isDesktop.value);
+  }
+
+  isDesktopView(): Observable<boolean> {
+    return this.isDesktop.asObservable().pipe(distinctUntilChanged());
   }
 
   openFirst() {
