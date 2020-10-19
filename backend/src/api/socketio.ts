0329209a5f6946a2b7fb4b67ebbf1bdacfc87756
Adam Cardenas <cevaris@gmail.com>
2020-10-19 15:53:27 +0000

    pathed + query params socketio

--- backend/src/api/socketio.ts
+++ backend/src/api/socketio.ts
@@ -3,9 +3,10 @@ import socketIO from 'socket.io';
 import { firehoseStatusReport } from './routes/public/firehose2';
 
 export function configureSocketIO(server: http.Server): void {
-    const io: socketIO.Server = socketIO.listen(server);
+    const io: socketIO.Server = socketIO.listen(server, { path: '/reports/firehose' });
 
     io.on('connection', function (socket: socketIO.Socket) {
+        console.log(socket.handshake.query);
         firehoseStatusReport(socket);
     });
 }
