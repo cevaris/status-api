2ce0145dc6d39aa69e3aa757fbdb7d83c32479ca
Adam Cardenas <cevaris@gmail.com>
2020-10-19 16:48:50 +0000

    working socketio solition

--- backend/src/api/routes/public/firehose2.ts
+++ backend/src/api/routes/public/firehose2.ts
@@ -1,64 +1,62 @@
 import socketIO from 'socket.io';
+import { isValidDate } from '../../../common/date';
 import { StatusReport, StatusReportStore } from '../../../common/storage/statusReport';
 import { Presenter } from '../../presenter';
 
 /**
  * DEV API
  */
-export function firehoseStatusReport(io: socketIO.Socket) {
-    const connection = `${new Date().toISOString()}-${io.client.id}`;
-    let isClientConnectionOpen = true;
+export function firehoseStatusReport(socket: socketIO.Socket) {
+    const eventType = 'status_report';
+    const connection = `${new Date().toISOString()}-${socket.client.id}`;
     let highWaterMark: Date = new Date();
 
-    // if (req.query.start_date) {
-    //     const now = new Date();
-    //     const queryStartDate = new Date(Date.parse(req.query.start_date));
+    const queryStartDate = socket.handshake.query.start_date;
+    if (queryStartDate) {
+        const now = new Date();
+        const startDate = new Date(Date.parse(queryStartDate));
 
-    //     if (!isValidDate(queryStartDate)) {
-    //         return res.status(400)
-    //             .json(Presenter.badRequest(`start_date '${req.query.start_date}' is invalid. Provide a valid ISO 8601 UTC format.`));
-    //     }
+        if (!isValidDate(startDate)) {
+            return socket.emit(eventType, Presenter.badRequest(`start_date '${queryStartDate}' is invalid. Provide a valid ISO 8601 UTC format.`));
+        }
 
-    //     if (now < queryStartDate) {
-    //         return res.status(400)
-    //             .json(Presenter.badRequest(`cannot use start_date value from the future, ${queryStartDate}`));
-    //     }
+        if (now < startDate) {
+            return socket.emit(eventType, Presenter.badRequest(`cannot use start_date value from the future, ${startDate.toISOString()}`));
+        }
 
-    //     const oldestStartDate = 1000 * 60 * 60 * 12;
-    //     if ((now.getTime() - queryStartDate.getTime()) > oldestStartDate) {
-    //         return res.status(400)
-    //             .json(Presenter.badRequest(`cannot use start_date value older than ${oldestStartDate / (1000 * 60 * 60)} hours from now`));
-    //     }
+        const hour = 1000 * 60 * 60;
+        const oldestStartDate = 12 * hour;
+        if ((now.getTime() - startDate.getTime()) > oldestStartDate) {
+            return socket.emit(eventType, Presenter.badRequest(`cannot use start_date value older than ${oldestStartDate / hour} hours from now`));
+        }
 
-    //     // valid start date
-    //     highWaterMark = queryStartDate;
-    // }
-
-    io.on('disconnect', function () {
-        console.log(connection, 'client disconnected');
-        isClientConnectionOpen = false;
-    });
+        // valid start date
+        highWaterMark = startDate;
+    }
 
     function streamReports() {
         const stream = StatusReportStore.streamReports(highWaterMark);
 
         stream
             .on('error', (error) => {
-                console.log(connection, 'end');
+                console.error(connection, error);
+                stream.end();
             })
             .on('data', (entity: StatusReport) => {
                 console.log(connection, 'entity', entity.name, entity.startDate);
                 highWaterMark = entity.startDate;
 
-                io.emit('status_report', Presenter.statusReports([entity]));
+                socket.emit(eventType, Presenter.statusReports([entity]));
             })
             .on('end', async () => {
-                if (isClientConnectionOpen) {
+                if (socket.connected) {
                     const sleepTimeMs = Math.floor(Math.random() * (10000 - 5000));
                     await new Promise(resolve => setTimeout(resolve, sleepTimeMs));
+
                     console.log(connection, 'stream new reports', highWaterMark);
                     streamReports();
                 } else {
+                    console.log(connection, 'client disconnected');
                     stream.end();
                 }
             });
